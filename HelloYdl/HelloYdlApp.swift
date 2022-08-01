900a722954891488058996c7dba5299092b45796
Changbeom Ahn <kewlbear@gmail.com>
2022-08-02 08:46:53 +0900

    Initial Commit

---
+++ HelloYdl/HelloYdlApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloYdlApp.swift
+//  HelloYdl
+//
+//  Created by Changbeom Ahn on 2022/08/02.
+//
+
+import SwiftUI
+
+@main
+struct HelloYdlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloYdlUITests/HelloYdlUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HelloYdlUITestsLaunchTests.swift
+//  HelloYdlUITests
+//
+//  Created by Changbeom Ahn on 2022/08/02.
+//
+
+import XCTest
+
+final class HelloYdlUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
